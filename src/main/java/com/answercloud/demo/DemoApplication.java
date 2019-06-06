7d07a9b90dcb744848fbac874fd91562c6730f1d
zhaodong <15216656849@163.com>
2019-06-06 15:35:22 +0800

    cat集成提交

--- src/main/java/com/answercloud/demo/DemoApplication.java
+++ src/main/java/com/answercloud/demo/DemoApplication.java
@@ -1,14 +1,15 @@
 package com.answercloud.demo;
 
 import com.spring4all.swagger.EnableSwagger2Doc;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ImportResource;
 
+@SpringBootApplication(scanBasePackages = {"com.answercloud.demo"})
 @EnableDiscoveryClient
-@SpringBootApplication
-@MapperScan("com.answercloud.demo.mapper")
+@ImportResource({"classpath:spring-mybatis.xml"})
+//@MapperScan("com.answercloud.demo.mapper")
 @EnableSwagger2Doc
 public class DemoApplication {
 
