f244702158652268542b1af9464d31dcd30a5332
zhaodong <15216656849@163.com>
2019-06-10 18:08:34 +0800

    cat集成提交

--- src/main/java/com/answercloud/demo/rest/UserController.java
+++ src/main/java/com/answercloud/demo/rest/UserController.java
@@ -5,6 +5,7 @@ import com.answercloud.demo.mapper.UserMapper;
 import com.answercloud.demo.vo.FrontUser;
 import com.dianping.cat.util.json.JsonArray;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,12 +28,32 @@ public class UserController {
     @RequestMapping(value = "/front/info", method = RequestMethod.POST)
     public FrontUser getUserInfo(Integer id) {
 
-        LOGGER.info("getUserInfo开始，入参:"+id);
+        LOGGER.info("getUserInfo开始，入参:" + id);
 
-        FrontUser frontUser = userManager.getUserById(id);
+        FrontUser frontUser = null;
+        try {
+            frontUser = userManager.getUserById(id);
 
-        LOGGER.info("getUserInfo返回参数："+id );
+            int value = 2 / 0;
+        } catch (Exception ex) {
+            LOGGER.error("getUserInfo出现错误", ex);
+        }
+
+        LOGGER.info("getUserInfo返回参数：" + id);
+
         return frontUser;
     }
 
+    @PostMapping(value = "/font/check")
+    public int checkError(Integer id) {
+        int result = 0;
+        LOGGER.info("check开始,入参：" + id);
+        try {
+            FrontUser frontUser = null;
+            frontUser.setId(id);
+        } catch (Exception ex) {
+            LOGGER.error("check出现错误", ex);
+        }
+        return result;
+    }
 }
